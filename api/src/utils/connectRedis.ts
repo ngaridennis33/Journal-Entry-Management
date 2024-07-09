bba36fd7107adfcb94e8e6cfc725af1569210979
ngaridennis33 <ngaridennis33@gmail.com>
2024-07-09 20:45:15 +0300

    Feat: Add redis connection

---
+++ api/src/utils/connectRedis.ts
@@ -0,0 +1,22 @@
+import { createClient } from 'redis';
+import log from "./logger"
+
+const redisUrl = 'redis://localhost:6379';
+
+const redisClient = createClient({
+    url: redisUrl,
+});
+
+const connectRedis = async () => {
+    try {
+        await redisClient.connect();
+        log.info('Redis client connected successfully');
+    } catch (error) {
+        log.info("Error Connecting to Redis", error)
+        setTimeout(connectRedis, 5000);
+    }
+};
+
+connectRedis();
+
+export default redisClient;
