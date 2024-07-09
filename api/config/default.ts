eae8a12ba8a5aaeb1d563b5c35c0020d08c4ae37
ngaridennis33 <ngaridennis33@gmail.com>
2024-07-09 19:58:40 +0300

    feat: Add the config folder to manage env settings

---
+++ api/config/default.ts
@@ -0,0 +1,10 @@
+export default {
+    redisCacheExpiresIn: 60,
+    refreshTokenExpiresIn: 60,
+    accessTokenExpiresIn: 60,
+    sessionTokenExpiresIn: 60,
+    logLevel: "info",
+    origin: "http://localhost:",
+    frontEndOrigin: "https://north-face-client.vercel.app/",
+    timezone: "Africa/Nairobi",
+}
