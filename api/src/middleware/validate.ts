d322823687a2948f3b19235be212a456342d3889
ngaridennis33 <ngaridennis33@gmail.com>
2024-07-09 20:34:21 +0300

    Feat: Add feature to validate Token

---
+++ api/src/middleware/validate.ts
@@ -0,0 +1,26 @@
+// middleware that will parse the schemas and return error messages to the user.
+import { AnyZodObject, ZodError } from 'zod'; //declaration and validation library designed to help you define and enforce data schemas.
+import { Request, Response, NextFunction } from 'express';
+
+export const validate =(schema: AnyZodObject) =>(
+    req: Request, 
+    res: Response,
+    next:NextFunction) => {
+    try {
+    schema.parse({
+        params: req.params,
+        query: req.query,
+        body: req.body,
+    });
+
+    next();
+    } catch (error) {
+    if (error instanceof ZodError) {
+        return res.status(400).json({
+        status: 'fail',
+        errors: error.errors,
+        });
+    }
+    next(error);
+    }
+};
