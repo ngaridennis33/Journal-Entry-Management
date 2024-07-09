f010582111f3220a8ceefddae915794a15085d1c
ngaridennis33 <ngaridennis33@gmail.com>
2024-07-09 20:48:01 +0300

    Feat: Add feat to log output

---
+++ api/src/utils/logger.ts
@@ -0,0 +1,20 @@
+import logger from "pino";
+import dayjs from "dayjs";
+import config from "config";
+
+
+const level = config.get<string>("logLevel");
+
+const log = logger({
+    // Adjust the pino-pretty
+    transport:{
+        target:'pino-pretty',
+    },
+    level,
+    base:{
+        pid:false,
+    },
+    timestamp: ()=> `"time": "${dayjs().format()}"`
+});
+
+export default log;
