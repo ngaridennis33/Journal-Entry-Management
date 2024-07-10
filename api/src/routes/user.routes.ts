64104789fde7628cb28ab8a64ec7780c1b9b1ced
ngaridennis33 <ngaridennis33@gmail.com>
2024-07-10 21:51:32 +0300

    refactor: Update get me handler route

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { requireUser } from '../middleware/require';
-import { deleteUserHandler, getAllUsersHandler, getMeHandler, updateUserHandler } from '../controllers/user.controller';
+import {getMeHandler } from '../controllers/user.controller';
 import { deserializeUser } from '../middleware/deserializeUser';
 
 
@@ -10,8 +10,5 @@ const router = express.Router();
 router.use(deserializeUser,requireUser);
 
 router.get('/me', getMeHandler);
-router.get('/', getAllUsersHandler);
-router.put('/id',updateUserHandler );
-router.patch('/delete/', deleteUserHandler);
 
 export default router;
