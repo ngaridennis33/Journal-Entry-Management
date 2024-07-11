c6b8c7d0f007e825aec786bcaaef4af1754269f1
ngaridennis33 <ngaridennis33@gmail.com>
2024-07-11 07:19:26 +0300

    Feat: Add category route

--- api/src/routes/journal.routes.ts
+++ api/src/routes/journal.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { validate } from "../middleware/validate";
-import { createJournalSchema, updateJournalSchema } from "../schema/journal.schema";
-import { createJournalHandler, deleteJournalHandler, getAllJournalsHandler, getSpecificJournalsHandler, updateJournalHandler } from "../controllers/journal.controllers";
+import { categorySchema, createJournalSchema, updateJournalSchema } from "../schema/journal.schema";
+import { createJournalHandler, deleteJournalHandler, getAllJournalsHandler, getJournalByCategoryHandler, getSpecificJournalsHandler, updateJournalHandler } from "../controllers/journal.controllers";
 import { deserializeUser } from "../middleware/deserializeUser";
 import { requireUser } from "../middleware/require";
 
@@ -12,7 +12,7 @@ router.post('/', validate(createJournalSchema), createJournalHandler);
 router.get('/', getAllJournalsHandler);
 router.get('/:id', getSpecificJournalsHandler);
 router.put('/:id',validate(updateJournalSchema), updateJournalHandler);
-router.get('/:category', getAllJournalsHandler);
+router.get('/category/:category',validate(categorySchema), getJournalByCategoryHandler);
 router.delete('/delete/:id', deleteJournalHandler);
 
 export default router;
